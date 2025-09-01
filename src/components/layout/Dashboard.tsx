53da667810e1674cc92f6561fefa32bbb5d3da99
Adegbokun Adefela <aadegbokun.2302873@stu.cu.edu.ng>
2025-09-01 16:33:16 +0100

    feat: add page load animation

--- src/components/layout/Dashboard.tsx
+++ src/components/layout/Dashboard.tsx
@@ -2,16 +2,18 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "@/components/layout/Sidebar";
 
 function Dashboard() {
-    return (
-        <div className="bg-white min-h-screen flex">
-            <div className="w-64 flex-shrink-0">
-                <Sidebar />
-            </div>
-            <div className="flex-1 overflow-auto">
-                <Outlet />
-            </div>
+  return (
+    <div className="animate-fadeInUp">
+      <div className="bg-white min-h-screen flex">
+        <div className="w-64 flex-shrink-0">
+          <Sidebar />
         </div>
-    );
+        <div className="flex-1 overflow-auto">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
